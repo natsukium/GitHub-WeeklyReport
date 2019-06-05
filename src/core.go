8e8614a4c1df7abc86fa355b4b891b8413a668e3
natsukium <tm.tomatoes@gmail.com>
2019-06-05 18:54:26 +0900

    Implement client

--- src/core.go
+++ src/core.go
@@ -1,7 +1,32 @@
 package ghpr
 
-import "fmt"
+	"context"
+	"log"
+	"os"
+
+	"github.com/google/go-github/github"
+	"github.com/joho/godotenv"
+	"golang.org/x/oauth2"
+)
 
 func Run() {
-	fmt.Println("Hello World!")
+	GetData()
+}
+
+func GetData() *github.Client {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	accessToken := os.Getenv("GITHUB_ACCESS_TOKEN")
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: accessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	return client
 }
